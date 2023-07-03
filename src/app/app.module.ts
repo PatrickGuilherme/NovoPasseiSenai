4232702bfb52d152141e6fcec2d66006bbd028fc
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2023-07-03 17:05:20 -0300

    Adição de componentes, e inputs do material angular
    /*Componentes do projeto */
    /*Importação para formulário reativo (não requisita instalação)*/
    /*Importações para os inputs personalizados (instala o angula material)*/
    /*Angula material*/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
+/*Importações essenciais*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+/*Componentes do projeto */
 import { BannerLogoComponent } from './Components/banner-logo/banner-logo.component';
-import { PaginaPrincipalComponent } from './Pages/pagina-principal/pagina-principal.component';
 import { FormularioComponent } from './Components/formulario/formulario.component';
+import { PaginaPrincipalComponent } from './Pages/pagina-principal/pagina-principal.component';
+import { AppComponent } from './app.component';
+
+/*Importação para formulário reativo (não requisita instalação)*/
+import { ReactiveFormsModule } from '@angular/forms';
+
+/*Importações para os inputs personalizados (instala o angula material)*/
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+/*Angula material*/
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +28,11 @@ import { FormularioComponent } from './Components/formulario/formulario.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
