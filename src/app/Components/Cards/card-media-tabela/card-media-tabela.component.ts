fff69759f39af9b7d980da1a0a130b1ae738902c
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2023-07-07 08:06:12 -0300

    Remoção de tabela angular material e exibição de  nível de dificuldade

--- src/app/Components/Cards/card-media-tabela/card-media-tabela.component.ts
+++ src/app/Components/Cards/card-media-tabela/card-media-tabela.component.ts
@@ -27,19 +27,51 @@ export class CardMediaTabelaComponent implements OnInit {
   }
   
   private PreencherTabela(){
-    let i = new Decimal(1.7);
-    let lim = new Decimal(6.9);
-    
-    for(i; i <= lim; i= i.plus(0.1))
+    let i = new Decimal(6.9);
+    let lim = new Decimal(1.7);
+    let operacaoMult:Decimal; 
+    let operacaoSub:Decimal;
+    let operacaoDiv:Decimal;
+
+    for(i; i >= lim; i= i.sub(0.1))
     {
-      let operacaoMult = Decimal.mul(6,i);
-      let operacaoSub = Decimal.sub(50, operacaoMult);
-      let operacaoDiv = operacaoSub.div(4).toFixed(1);
+      operacaoMult = Decimal.mul(6,i);
+      operacaoSub = Decimal.sub(50, operacaoMult);
+      operacaoDiv = operacaoSub.div(4);
 
       this.notasTabela.push(
-        new Media_ProvaFinal(i.toString(), operacaoDiv)
+        new Media_ProvaFinal(i.toString(), operacaoDiv.toFixed(1))
       );
     }
+  }
+
+  public GetIconeDificuldade(){
+
+    let operacaoDiv = this.estudante.CalcularNotaParaFinal();
+    let icone:string;
+
+    if(operacaoDiv <= 2.9){
+      icone = "nivel1.jpg"
+    }else if(operacaoDiv <= (3.8)){
+      icone = "nivel2.jpg"
+    }else if(operacaoDiv <= (3.9)){
+      icone = "nivel3.jpg"
+    }else if(operacaoDiv <= (4.9)){
+      icone = "nivel4.jpg"
+    }else if(operacaoDiv <= (5.9)){
+      icone = "nivel5.jpg"
+    }else if(operacaoDiv <= (6.8)){
+      icone = "nivel6.jpg"
+    }else if(operacaoDiv <= (7.9)){
+      icone = "nivel7.jpg"
+    }else if(operacaoDiv <= (8.9)){
+      icone = "nivel8.jpg"
+    }else if(operacaoDiv <= (9.8)){
+      icone = "nivel9.jpg"
+    }else{
+      icone = "nivel10.jpg"
+    }
+    return "../../../../assets/" + icone;
   }
 
   public VerifyEstudanteObj():boolean{
